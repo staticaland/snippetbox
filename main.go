fc0d5f6f40a9520af22dbe8bf5f63acd436b96cc
Anders K. Pettersen <staticaland@users.noreply.github.com>
2022-09-05 01:07:09 +0200

    feat: ID for view handler

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-    "log"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -17,10 +19,28 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Display a specific snippet..."))
+    // Extract the value of the id parameter from the query string and try to
+    // convert it to an integer using the strconv.Atoi() function. If it can't
+    // be converted to an integer, or the value is less than 1, we return a 404 page
+    // not found response.
+    id, err := strconv.Atoi(r.URL.Query().Get("id"))
+    if err != nil || id < 1 {
+        http.NotFound(w, r)
+        return
+    }
+
+    // Use the fmt.Fprintf() function to interpolate the id value with our response
+    // and write it to the http.ResponseWriter.
+    fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
+
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+        w.Header().Set("Allow", http.MethodPost)
+        http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
     w.Write([]byte("Create a new snippet..."))
 }
 
