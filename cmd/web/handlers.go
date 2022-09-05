c3a0d9ae1767d19b48454ce7b2228b2329e5da76
Anders K. Pettersen <staticaland@users.noreply.github.com>
2022-09-05 02:41:01 +0200

    feat: Partials

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -14,17 +14,13 @@ func home(w http.ResponseWriter, r *http.Request) {
         return
     }
 
-    // Initialize a slice containing the paths to the two files. It's important
-    // to note that the file containing our base template must be the *first*
-    // file in the slice.
+    // Include the navigation partial in the template files.
     files := []string{
         "./ui/html/base.tmpl",
+        "./ui/html/partials/nav.tmpl",
         "./ui/html/pages/home.tmpl",
     }
 
-    // Use the template.ParseFiles() function to read the files and store the
-    // templates in a template set. Notice that we can pass the slice of file
-    // paths as a variadic parameter?
     ts, err := template.ParseFiles(files...)
     if err != nil {
         log.Print(err.Error())
@@ -32,8 +28,6 @@ func home(w http.ResponseWriter, r *http.Request) {
         return
     }
 
-    // Use the ExecuteTemplate() method to write the content of the "base"
-    // template as the response body.
     err = ts.ExecuteTemplate(w, "base", nil)
     if err != nil {
         log.Print(err.Error())
