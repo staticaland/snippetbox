fa4f12d0768333f5bcd2432db80fe42458e6427a
Anders K. Pettersen <staticaland@users.noreply.github.com>
2022-09-07 19:01:29 +0200

    Finished 3.4

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,14 +3,13 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
     if r.URL.Path != "/" {
-        http.NotFound(w, r)
+		app.notFound(w)
         return
     }
 
@@ -23,32 +22,30 @@ func home(w http.ResponseWriter, r *http.Request) {
 
     ts, err := template.ParseFiles(files...)
     if err != nil {
-        log.Print(err.Error())
-        http.Error(w, "Internal Server Error", 500)
+		app.serverError(w, err)
         return
     }
 
     err = ts.ExecuteTemplate(w, "base", nil)
     if err != nil {
-        log.Print(err.Error())
-        http.Error(w, "Internal Server Error", 500)
+		app.serverError(w, err)
     }
 }
 
-func snippetView(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
     id, err := strconv.Atoi(r.URL.Query().Get("id"))
     if err != nil || id < 1 {
-        http.NotFound(w, r)
+		app.notFound(w)
         return
     }
 
     fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
     if r.Method != http.MethodPost {
         w.Header().Set("Allow", http.MethodPost)
-        http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+        app.clientError(w, http.StatusMethodNotAllowed)
         return
     }
 
