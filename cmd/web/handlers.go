ee1a2ce4c45e929ced2a90ece17e620c9f1998de
Anders K. Pettersen <staticaland@users.noreply.github.com>
2022-09-08 17:01:55 +0200

    feat: 4.6 - Insert snippet

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -49,5 +49,17 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
         return
     }
 
-    w.Write([]byte("Create a new snippet..."))
+	title := "O snail"
+    content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+	expires := 7
+
+	id, err := app.snippets.Insert(title, content, expires)
+
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,11 +8,13 @@ import (
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/staticaland/snippetbox/internal/models"
 )
 
 type application struct {
 	errorLog *log.Logger
 	infoLog *log.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
@@ -35,6 +37,7 @@ func main() {
 	app := &application{
 		errorLog: errorLog,
 		infoLog: infoLog,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	srv := &http.Server{
