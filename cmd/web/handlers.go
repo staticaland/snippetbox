9a4b0c4fd965162c55b2db958a7bf8115c0b3a05
Anders K. Pettersen <staticaland@users.noreply.github.com>
2022-09-09 12:58:56 +0200

    feat: 5.1 - Displaying dynamic data
    - Added a template for viewing snippets that receives template data
    - Rendering multiple pieces of data by wrapping data in a holding structure

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,7 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
-	// "html/template"
+	"html/template"
 	"net/http"
 	"strconv"
 
@@ -62,7 +62,27 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-    fmt.Fprintf(w, "%+v", snippet)
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/partials/nav.tmpl",
+		"./ui/html/pages/view.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	data := &templateData{
+		Snippet: snippet,
+	}
+
+	err = ts.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		app.serverError(w, err)
+	}
+
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
---
+++ cmd/web/templates.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/staticaland/snippetbox/internal/models"
+
+type templateData struct {
+	Snippet *models.Snippet
+}
