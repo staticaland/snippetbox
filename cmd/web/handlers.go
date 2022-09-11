75a3416dd65564c02053b89ac755599d56ecb9f3
Anders K. Pettersen <staticaland@users.noreply.github.com>
2022-09-12 01:52:41 +0200

    feat: 7.2 - Clean URLs and method-based routing

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -7,14 +7,12 @@ import (
 	"strconv"
 
 	"github.com/staticaland/snippetbox/internal/models"
+
+    "github.com/julienschmidt/httprouter"
 )
 
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-    if r.URL.Path != "/" {
-        app.notFound(w)
-        return
-    }
 
     snippets, err := app.snippets.Latest()
     if err != nil {
@@ -30,7 +28,10 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
-    id, err := strconv.Atoi(r.URL.Query().Get("id"))
+
+	params := httprouter.ParamsFromContext(r.Context())
+
+    id, err := strconv.Atoi(params.ByName("id"))
     if err != nil || id < 1 {
 		app.notFound(w)
         return
@@ -54,6 +55,10 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display the form for creating a snippet..."))
+}
+
+func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
     if r.Method != http.MethodPost {
         w.Header().Set("Allow", http.MethodPost)
         app.clientError(w, http.StatusMethodNotAllowed)
@@ -71,6 +76,6 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,20 +3,28 @@ package main
 import (
     "net/http"
 
+    "github.com/julienschmidt/httprouter"
     "github.com/justinas/alice"
 )
 
 func (app *application) routes() http.Handler {
-	mux := http.NewServeMux()
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-    mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+    router := httprouter.New()
 
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.notFound(w)
+	})
 
-	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+    fileServer := http.FileServer(http.Dir("./ui/static/"))
+    router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
-	return standard.Then(mux)
+    router.HandlerFunc(http.MethodGet, "/", app.home)
+    router.HandlerFunc(http.MethodGet, "/snippet/view/:id", app.snippetView)
+    router.HandlerFunc(http.MethodGet, "/snippet/create", app.snippetCreate)
+    router.HandlerFunc(http.MethodPost, "/snippet/create", app.snippetCreatePost)
+
+    standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+
+    return standard.Then(router)
+
 }
