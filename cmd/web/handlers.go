620e60c881c720fef2280d17212f9f0b51eada53
Anders K. Pettersen <staticaland@users.noreply.github.com>
2022-09-15 00:49:14 +0200

    feat: 11.5 - User logout
    Remove the context key and add a flash message to the context.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -217,5 +217,15 @@ func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
 
 func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Fprintln(w, "Logout the user...")
+	err := app.sessionManager.RenewToken(r.Context())
+	if err != nil {
+		app.serverError(w, err)
+	}
+
+	app.sessionManager.Remove(r.Context(), "authenticatedUserID")
+
+	app.sessionManager.Put(r.Context(), "flash", "You've been logged out successfully!")
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+
 }
